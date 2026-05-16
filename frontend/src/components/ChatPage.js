0010a86813ca77d4c5c8ad6bd7f9e419ceca891b
Quy20061 <tranquy190806@gmail.com>
2026-05-16 14:36:08 +0700

    Khởi tạo dự án hoặc commit 12

--- frontend/src/components/ChatPage.js
+++ frontend/src/components/ChatPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useCallback } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import { useSocket } from '../contexts/SocketContext';
 import { useAuth } from '../contexts/AuthContext';
